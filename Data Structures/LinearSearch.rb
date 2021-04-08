107883b447747a6ae9b83ab7e472630323856a7b
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-04-08 13:25:10 +0300

    Application

---
+++ Data Structures/LinearSearch.rb
@@ -0,0 +1,11 @@
+def linear_search(array, key)
+    if array.index(key).nil?
+        return - 1
+    else
+        return "#{key} at index #{array.index(key)}"
+    end
+end
+
+arr = [7, 6, 25, 19, 8, 14, 3, 16, 2, 0]
+key = 7
+p linear_search(arr, key)
